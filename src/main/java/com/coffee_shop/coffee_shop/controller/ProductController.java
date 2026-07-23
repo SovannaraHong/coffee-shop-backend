efc187693f1f601d8b7ac3e02a0165111f5b772c
SovannaraHong <rapewpew372@gmail.com>
2026-07-23 19:09:56 +0700

    feature product

--- src/main/java/com/coffee_shop/coffee_shop/controller/ProductController.java
+++ src/main/java/com/coffee_shop/coffee_shop/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.coffee_shop.coffee_shop.entity.Product;
 import com.coffee_shop.coffee_shop.mapper.ProductMapper;
 import com.coffee_shop.coffee_shop.service.ProductService;
 import com.coffee_shop.coffee_shop.service.S3Service;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController()
@@ -56,6 +58,55 @@ public class ProductController {
         ProductResponse response = productService.updateImage(id, url);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAll() {
+        return ResponseEntity.ok().body(productService.getAll());
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<ProductResponse> update(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(productService.update(id, request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getById(@PathVariable Long id) {
+        Product proId = productService.findById(id);
+        ProductResponse response = productMapper.toResponse(proId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<ProductResponse> changeStatus(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                productService.changeProductStatus(id)
+
+        );
+    }
+
+    @GetMapping("/{id}/category")
+    public ResponseEntity<List<ProductResponse>> findProductByCategoryId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(productService.findProductByCategoryId(id));
+    }
+
+    @GetMapping("/feature")
+    public ResponseEntity<List<ProductResponse>> findFeatureProducts() {
+        return ResponseEntity.ok().body(productService.findFeaturedProducts());
+    }
+
+    @GetMapping("/new")
+    public ResponseEntity<List<ProductResponse>> findNewestProducts() {
+        return ResponseEntity.ok().body(productService.findNewestProducts());
     }
 
 
