08fbb9897bbd82f036e841bfb45a6df9c402d183
SovannaraHong <rapewpew372@gmail.com>
2026-09-02 20:04:05 +0700

    improve order with intergration payment

--- src/main/java/com/coffee_shop/coffee_shop/CoffeeShopApplication.java
+++ src/main/java/com/coffee_shop/coffee_shop/CoffeeShopApplication.java
@@ -18,7 +18,9 @@ package com.coffee_shop.coffee_shop;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CoffeeShopApplication {
 
