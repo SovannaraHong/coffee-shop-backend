42bb1c463b78f7bba824e2e16d014167169cc392
SovannaraHong <rapewpew372@gmail.com>
2026-09-04 21:28:14 +0700

    liquidbase

--- src/main/java/com/coffee_shop/coffee_shop/config/AuditorAwareImpl.java
+++ src/main/java/com/coffee_shop/coffee_shop/config/AuditorAwareImpl.java
@@ -1,26 +1,37 @@
 package com.coffee_shop.coffee_shop.config;
 
+import com.coffee_shop.coffee_shop.entity.User;
+import com.coffee_shop.coffee_shop.repository.UserRepository;
+import com.coffee_shop.coffee_shop.security.auth.AuthUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component("auditorAware")
-public class AuditorAwareImpl implements AuditorAware<String> {
+@RequiredArgsConstructor
+public class AuditorAwareImpl implements AuditorAware<User> {
+
+    private final UserRepository userRepository;
+
     @Override
-    public Optional<String> getCurrentAuditor() {
+    public Optional<User> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null
+                || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        if (authentication.getPrincipal() instanceof AuthUser authUser) {
+            return userRepository.findById(authUser.getId());
+        }
+
         return Optional.empty();
     }
-
-//    @Override
-//    public Optional<String> getCurrentAuditor() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        if (authentication == null || !authentication.isAuthenticated()
-//                || "anonymousUser".equals(authentication.getPrincipal())) {
-//            return Optional.of("SYSTEM");
-//        }
-//
-//        return Optional.of(authentication.getName());
-//    }
 }
